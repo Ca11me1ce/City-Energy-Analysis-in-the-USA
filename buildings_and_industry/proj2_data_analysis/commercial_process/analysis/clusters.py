b26b08fb4b546212153712368b61521c25cd726b
Yang Chen <yangchen2048@gmail.com>
2019-11-02 14:52:05 -0400

    Add pca plot

--- buildings_and_industry/proj2_data_analysis/commercial_process/analysis/clusters.py
+++ buildings_and_industry/proj2_data_analysis/commercial_process/analysis/clusters.py
@@ -1,3 +1,7 @@
+# K Means
+# Hierarchical - Ward
+# DBScan
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
@@ -5,6 +9,7 @@ from sklearn.cluster import KMeans
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import silhouette_samples, silhouette_score
+from sklearn import decomposition
 
 def readData(file_name):
     df=pd.read_csv(file_name, sep=',')
@@ -44,6 +49,8 @@ def clusterKMeans(df):
 
     # for i in range(len(key_ls)-1):
     #     print(pd.crosstab(cluster_labels, df[key_ls[i]]))
+    plotPCA(normalizedDataFrame, 'KMeans', cluster_labels)
+
 
 def clusterWard(df):
     # Hierarchical cluster - ward
@@ -76,6 +83,7 @@ def clusterWard(df):
 
     # for i in range(len(key_ls)-1):
     #     print(pd.crosstab(cluster_labels, df[key_ls[i]]))
+    plotPCA(normalizedDataFrame, 'Ward', cluster_labels)
 
 def clusterDBScan(df):
     df=df.drop(df.columns[[0, 1]], axis=1)
@@ -99,9 +107,40 @@ def clusterDBScan(df):
     silhouette_avg = silhouette_score(normalizedDataFrame, cluster_labels)
     print("The average silhouette_score is :", silhouette_avg)
 
+    plotPCA(normalizedDataFrame, 'DBScan', cluster_labels)
+
+def plotPCA(normalizedDataFrame, mode, cluster_labels):
+    #####
+    # PCA
+    # Let's convert our high dimensional data to 2 dimensions
+    # using PCA
+    pca2D = decomposition.PCA(2)
+
+    # Turn the NY Times data into two columns with PCA
+    pca2D = pca2D.fit(normalizedDataFrame)
+    plot_columns = pca2D.transform(normalizedDataFrame)
+
+    # This shows how good the PCA performs on this dataset
+    print(mode+' PCA 2D Explained Varience : ', pca2D.explained_variance_)
+
+    # Plot using a scatter plot and shade by cluster label
+    plt.scatter(x=plot_columns[:,0], y=plot_columns[:,1], c=cluster_labels)
+    plt.title(mode+" : 2-dimensional scatter plot using PCA")
+
+    # Write to file
+    plt.savefig(mode+"-pca2D.png")
+
+    # Clear plot
+    plt.clf()
+
 
 if __name__ == "__main__":
+
+    # Read data
     df=readData('cleaned_energy_commercial.csv')
+
+    # #--------------------------------------------------
+    # # Histogram of Commercial Data
     # df.hist()
     # fig1=plt.gcf()
 
@@ -110,13 +149,20 @@ if __name__ == "__main__":
     # fig1.savefig('hist.jpg')
     # plt.clf()
 
-    df1=df.copy()
-    clusterKMeans(df1)
+    # # Clusters
+    # # K Means
+    # # Hierarchical - Ward
+    # # DBScan
+    # df1=df.copy()
+    # clusterKMeans(df1)
 
-    df2=df.copy()
-    clusterWard(df2)
+    # df2=df.copy()
+    # clusterWard(df2)
 
-    df3=df.copy()
-    clusterDBScan(df3)
+    # df3=df.copy()
+    # clusterDBScan(df3)
+    # #-------------------------------------------------
+
+    # Assciation rules
 
     
