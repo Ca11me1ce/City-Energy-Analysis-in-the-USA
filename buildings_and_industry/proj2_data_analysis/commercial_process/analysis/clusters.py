6b2be9f99fe27bdf2aaa7e3f167afba3b79c3f73
Yang Chen <yangchen2048@gmail.com>
2019-11-03 07:36:53 -0500

    Add t-test, association rules, correlation in analysis

--- buildings_and_industry/proj2_data_analysis/commercial_process/analysis/clusters.py
+++ buildings_and_industry/proj2_data_analysis/commercial_process/analysis/clusters.py
@@ -1,15 +1,21 @@
 # K Means
 # Hierarchical - Ward
 # DBScan
+# Association Rules
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.cluster import DBSCAN
 from sklearn.metrics import silhouette_samples, silhouette_score
 from sklearn import decomposition
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+pd.options.display.width=0
 
 def readData(file_name):
     df=pd.read_csv(file_name, sep=',')
@@ -18,6 +24,21 @@ def readData(file_name):
 def openFile(file_name):
     f=open(file_name, 'w')
     return f
+
+def correlation(myData):
+    df=pd.DataFrame()
+    df['elec_mwh']=myData['elec_mwh']
+    df['gas_mcf']=myData['gas_mcf']
+    df['num_establishments']=myData['num_establishments']
+
+    _corr=df.corr()
+    print(_corr)
+    # Plot scatter of dataframe of correlation.
+    scatter_matrix(df)
+    fig1=plt.gcf()
+    plt.show()
+    fig1.savefig('correlation.png')
+    plt.clf()
 
 def clusterKMeans(df):
     # K-Means
@@ -31,26 +52,25 @@ def clusterKMeans(df):
 
     df=pd.concat(concat_ls, axis=1, keys=key_ls)
     x=df.values
-    min_max_scaler = preprocessing.MinMaxScaler()
-    x_scaled = min_max_scaler.fit_transform(x)
-    normalizedDataFrame = pd.DataFrame(x_scaled)
+    min_max_scaler=preprocessing.MinMaxScaler()
+    x_scaled=min_max_scaler.fit_transform(x)
+    normalizedDataFrame=pd.DataFrame(x_scaled)
     # print(normalizedDataFrame[:10])
 
-    k = 5
-    kmeans = KMeans(n_clusters=k)
-    cluster_labels = kmeans.fit_predict(normalizedDataFrame)
+    k=5
+    kmeans=KMeans(n_clusters=k)
+    cluster_labels=kmeans.fit_predict(normalizedDataFrame)
 
-    silhouette_avg = silhouette_score(normalizedDataFrame, cluster_labels)
+    silhouette_avg=silhouette_score(normalizedDataFrame, cluster_labels)
     print("For n_clusters =", k, "The average silhouette_score is :", silhouette_avg)
 
-    # centroids = kmeans.cluster_centers_
+    # centroids=kmeans.cluster_centers_
     # print(cluster_labels)
     # print(centroids)
 
     # for i in range(len(key_ls)-1):
     #     print(pd.crosstab(cluster_labels, df[key_ls[i]]))
     plotPCA(normalizedDataFrame, 'KMeans', cluster_labels)
-
 
 def clusterWard(df):
     # Hierarchical cluster - ward
@@ -64,20 +84,20 @@ def clusterWard(df):
 
     df=pd.concat(concat_ls, axis=1, keys=key_ls)
     x=df.values
-    min_max_scaler = preprocessing.MinMaxScaler()
-    x_scaled = min_max_scaler.fit_transform(x)
-    normalizedDataFrame = pd.DataFrame(x_scaled)
+    min_max_scaler=preprocessing.MinMaxScaler()
+    x_scaled=min_max_scaler.fit_transform(x)
+    normalizedDataFrame=pd.DataFrame(x_scaled)
     # print(normalizedDataFrame[:10])
 
     k=5
     ward=AgglomerativeClustering(n_clusters=k, linkage='ward')
 
-    cluster_labels = ward.fit_predict(normalizedDataFrame)
+    cluster_labels=ward.fit_predict(normalizedDataFrame)
 
-    silhouette_avg = silhouette_score(normalizedDataFrame, cluster_labels)
+    silhouette_avg=silhouette_score(normalizedDataFrame, cluster_labels)
     print("For n_clusters =", k, "The average silhouette_score is :", silhouette_avg)
 
-    # centroids = ward.cluster_centers_
+    # centroids=ward.cluster_centers_
     # print(cluster_labels)
     # print(centroids)
 
@@ -96,15 +116,15 @@ def clusterDBScan(df):
 
     df=pd.concat(concat_ls, axis=1, keys=key_ls)
     x=df.values
-    min_max_scaler = preprocessing.MinMaxScaler()
-    x_scaled = min_max_scaler.fit_transform(x)
-    normalizedDataFrame = pd.DataFrame(x_scaled)
+    min_max_scaler=preprocessing.MinMaxScaler()
+    x_scaled=min_max_scaler.fit_transform(x)
+    normalizedDataFrame=pd.DataFrame(x_scaled)
     # print(normalizedDataFrame[:10])
 
     dbs=DBSCAN(eps=0.3, min_samples=10)
-    cluster_labels = dbs.fit_predict(normalizedDataFrame)
+    cluster_labels=dbs.fit_predict(normalizedDataFrame)
 
-    silhouette_avg = silhouette_score(normalizedDataFrame, cluster_labels)
+    silhouette_avg=silhouette_score(normalizedDataFrame, cluster_labels)
     print("The average silhouette_score is :", silhouette_avg)
 
     plotPCA(normalizedDataFrame, 'DBScan', cluster_labels)
@@ -114,11 +134,11 @@ def plotPCA(normalizedDataFrame, mode, cluster_labels):
     # PCA
     # Let's convert our high dimensional data to 2 dimensions
     # using PCA
-    pca2D = decomposition.PCA(2)
+    pca2D=decomposition.PCA(2)
 
     # Turn the NY Times data into two columns with PCA
-    pca2D = pca2D.fit(normalizedDataFrame)
-    plot_columns = pca2D.transform(normalizedDataFrame)
+    pca2D=pca2D.fit(normalizedDataFrame)
+    plot_columns=pca2D.transform(normalizedDataFrame)
 
     # This shows how good the PCA performs on this dataset
     print(mode+' PCA 2D Explained Varience : ', pca2D.explained_variance_)
@@ -133,21 +153,44 @@ def plotPCA(normalizedDataFrame, mode, cluster_labels):
     # Clear plot
     plt.clf()
 
+def associationRules(myData):
+    df=pd.DataFrame()
+    attrs=['elec_class', 'gas_class']
+    df['elec_class']=myData['elec_bin_group']
+    df['gas_class']=myData['gas_bin_group']
 
-if __name__ == "__main__":
+    # Convert group to class representation
+    for attr in attrs:
+        df[attr][df[attr]==1]=attr+"_normal"
+        df[attr][df[attr]==2]=attr+"_high"
+        df[attr][df[attr]==3]=attr+"_very_high"
+
+    valueArray=df.values
+    _encoder=TransactionEncoder()
+    encoder_labels=_encoder.fit(valueArray).transform(valueArray)
+    df=pd.DataFrame(encoder_labels, columns=_encoder.columns_)
+    frequent_itemsets=apriori(df, min_support=0.05, use_colnames=True) 
+
+    print(frequent_itemsets)
+    result=association_rules(frequent_itemsets, metric="confidence", min_threshold=0.6)
+    print(result)
+
+
+if __name__=="__main__":
 
     # Read data
     df=readData('cleaned_energy_commercial.csv')
 
-    #--------------------------------------------------
     # Histogram of Commercial Data
+    # Correlation
     df.hist()
-    fig1=plt.gcf()
-
     plt.title('Histograme of Commercial Data')
+    fig1=plt.gcf()
     plt.show()
     fig1.savefig('hist.jpg')
     plt.clf()
+
+    correlation(df)
 
     # Clusters
     # K Means
@@ -161,8 +204,8 @@ if __name__ == "__main__":
 
     df3=df.copy()
     clusterDBScan(df3)
-    #-------------------------------------------------
 
-    # Assciation rules
+    # Assciation Rules
+    associationRules(df)
 
     
--- buildings_and_industry/proj2_data_analysis/commercial_process/analysis/prediction.py
+++ buildings_and_industry/proj2_data_analysis/commercial_process/analysis/prediction.py
@@ -13,8 +13,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LinearRegression
-
-
+from scipy import stats
 
 
 def readData(file_name):
@@ -25,15 +24,16 @@ def openFile(file_name):
     f=open(file_name, 'w')
     return f
 
-def run_knn(X_train, Y_train, X_validate, Y_validate, num_instances):
+def runKNN(X_train, Y_train, X_validate, Y_validate, num_instances):
+    print('KNN in Validate Data: ')
     # f.write('\nKNN in Validate Data: \n')
     
     # Make predictions on validation dataset
-    cart = KNeighborsClassifier()
+    cart=KNeighborsClassifier()
     cart.fit(X_train, Y_train)
 
     # Get the predictions of validate data
-    predictions = cart.predict(X_validate)   
+    predictions=cart.predict(X_validate)   
 
     # Analyze and evaluate the predictions and models
     accu_score=accuracy_score(Y_validate, predictions)
@@ -48,15 +48,16 @@ def run_knn(X_train, Y_train, X_validate, Y_validate, num_instances):
     print(class_report)
     # f.write('Classification Report: \n'+ str(class_report)+'\n')
 
-def run_cart(X_train, Y_train, X_validate, Y_validate, num_instances):
+def runCart(X_train, Y_train, X_validate, Y_validate, num_instances):
     # f.write('\nCart in Validate Data: \n')
+    print('Cart in Validate Data: ')
     
     # Make predictions on validation dataset
-    cart = DecisionTreeClassifier()
+    cart=DecisionTreeClassifier()
     cart.fit(X_train, Y_train)
 
     # Get the predictions of validate data
-    predictions = cart.predict(X_validate)   
+    predictions=cart.predict(X_validate)   
 
     # Analyze and evaluate the predictions and models
     accu_score=accuracy_score(Y_validate, predictions)
@@ -71,15 +72,16 @@ def run_cart(X_train, Y_train, X_validate, Y_validate, num_instances):
     print(class_report)
     # f.write('Classification Report: \n'+ str(class_report)+'\n')
 
-def run_nb(X_train, Y_train, X_validate, Y_validate, num_instances):
+def runNB(X_train, Y_train, X_validate, Y_validate, num_instances):
     # f.write('\nNaive Bayes in Validate Data: \n')
+    print('Naive Bayes in Validate Data: ')
 
     # Make predictions on validation dataset
-    nb = GaussianNB()    # model
+    nb=GaussianNB()    # model
     nb.fit(X_train, Y_train)
 
     # Get the predictions of validate data
-    predictions = nb.predict(X_validate)   
+    predictions=nb.predict(X_validate)   
 
     # Analyze and evaluate the predictions and models
     accu_score=accuracy_score(Y_validate, predictions)
@@ -94,15 +96,16 @@ def run_nb(X_train, Y_train, X_validate, Y_validate, num_instances):
     print(class_report)
     # f.write('Classification Report: \n'+ str(class_report)+'\n')
 
-def run_svm(X_train, Y_train, X_validate, Y_validate, num_instances):
+def runSVM(X_train, Y_train, X_validate, Y_validate, num_instances):
     # f.write('\nSVC in Validate Data: \n')
+    print('SVM in Validate Data: ')
     
     # Make predictions on validation dataset
-    _svc = SVC(gamma='scale')
+    _svc=SVC(gamma='scale')
     _svc.fit(X_train, Y_train)
 
     # Get the predictions of validate data
-    predictions = _svc.predict(X_validate)   
+    predictions=_svc.predict(X_validate)   
 
     # Analyze and evaluate the predictions and models
     accu_score=accuracy_score(Y_validate, predictions)
@@ -117,15 +120,16 @@ def run_svm(X_train, Y_train, X_validate, Y_validate, num_instances):
     print(class_report)
     # f.write('Classification Report: \n'+ str(class_report)+'\n')
 
-def run_rf(X_train, Y_train, X_validate, Y_validate, num_instances):
-    # f.write('\nKNN in Validate Data: \n')
+def runRandomForest(X_train, Y_train, X_validate, Y_validate, num_instances):
+    # f.write('\nRandom Forest in Validate Data: \n')
+    print('Random Forest in Validate Data: ')
     
     # Make predictions on validation dataset
-    cart = RandomForestClassifier(n_estimators=10)
+    cart=RandomForestClassifier(n_estimators=10)
     cart.fit(X_train, Y_train)
 
     # Get the predictions of validate data
-    predictions = cart.predict(X_validate)   
+    predictions=cart.predict(X_validate)   
 
     # Analyze and evaluate the predictions and models
     accu_score=accuracy_score(Y_validate, predictions)
@@ -140,32 +144,32 @@ def run_rf(X_train, Y_train, X_validate, Y_validate, num_instances):
     print(class_report)
     # f.write('Classification Report: \n'+ str(class_report)+'\n')
 
-def testClassfier(myData):
+def predictByClassfiers(myData):
     # Separate training and final validation data set. First remove class
     # label from data (X). Setup target class (Y)
     # Then make the validation set 20% of the entire
     # set of labeled data (X_validate, Y_validate)
-    valueArray = myData.values
-    X = valueArray[:, 0:5]
-    Y = valueArray[:, 5]
-    test_size = 0.20
-    seed = 7
-    X_train, X_validate, Y_train, Y_validate = train_test_split(X, Y, test_size=test_size, random_state=seed)
+    valueArray=myData.values
+    X=valueArray[:, 0:5]
+    Y=valueArray[:, 5]
+    test_size=0.20
+    seed=7
+    X_train, X_validate, Y_train, Y_validate=train_test_split(X, Y, test_size=test_size, random_state=seed)
 
     # Setup 10-fold cross validation to estimate the accuracy of different models
     # Split data into 10 parts
     # Test options and evaluation metric
-    num_folds = 10
-    num_instances = len(X_train)
-    seed = 7
-    scoring = 'accuracy'
+    num_folds=10
+    num_instances=len(X_train)
+    seed=7
+    scoring='accuracy'
 
     ######################################################
     # Use different algorithms to build models
     ######################################################
 
     # Add each algorithm and its name to the model array
-    models = []
+    models=[]
     models.append(('KNN', KNeighborsClassifier()))
     models.append(('CART', DecisionTreeClassifier()))
     models.append(('Naive Bayes', GaussianNB()))
@@ -174,34 +178,32 @@ def testClassfier(myData):
 
     # Evaluate each model, add results to a results array,
     # Print the accuracy results (remember these are averages and std
-    results = []
-    names = []
+    results=[]
+    names=[]
     for name, model in models:
-        kfold = KFold(n_splits=num_folds, random_state=seed, shuffle=False)
-        cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+        kfold=KFold(n_splits=num_folds, random_state=seed, shuffle=False)
+        cv_results=cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
         results.append(cv_results)
         names.append(name)
-        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        msg="%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
         print(msg)
 
-    run_knn(X_train, Y_train, X_validate, Y_validate, num_instances)
-    run_cart(X_train, Y_train, X_validate, Y_validate, num_instances)
-    run_nb(X_train, Y_train, X_validate, Y_validate, num_instances)
-    run_svm(X_train, Y_train, X_validate, Y_validate, num_instances)
-    run_rf(X_train, Y_train, X_validate, Y_validate, num_instances)
-
-    
-
+    runKNN(X_train, Y_train, X_validate, Y_validate, num_instances)
+    runCart(X_train, Y_train, X_validate, Y_validate, num_instances)
+    runNB(X_train, Y_train, X_validate, Y_validate, num_instances)
+    runSVM(X_train, Y_train, X_validate, Y_validate, num_instances)
+    runRandomForest(X_train, Y_train, X_validate, Y_validate, num_instances)
+   
 def linearRegression(myData):
-    valueArray = myData.values
+    valueArray=myData.values
     j=1
     for i in range(5):
-        X = valueArray[:, i:j]
-        Y = valueArray[:, 5]
+        X=valueArray[:, i:j]
+        Y=valueArray[:, 5]
 
-        linear_regressor = LinearRegression()  # create object for the class
+        linear_regressor=LinearRegression()  # create object for the class
         linear_regressor.fit(X, Y)  # perform linear regression
-        Y_pred = linear_regressor.predict(X)  # make predictions
+        Y_pred=linear_regressor.predict(X)  # make predictions
         j=j+1
         # print(Y_pred)
         # print(len(X[0]))
@@ -211,6 +213,10 @@ def linearRegression(myData):
         plt.show()
         plt.clf()
 
+def tTest(myData):
+    result = stats.ttest_ind(myData['elec_bin_group'], myData['gas_bin_group'])
+    print("The result of t-test is: ", result)
+    print("The p-valie is greater than 0.05? ", result.pvalue > 0.05)
 
 
 if __name__ == "__main__":
@@ -233,9 +239,12 @@ if __name__ == "__main__":
     linearRegression(gas_df)
 
     print('\nClassfier for Elec Dataframe: ')
-    testClassfier(elec_df)
+    predictByClassfiers(elec_df)
     print('\nClassfier for Gas Dataframe: ')
-    testClassfier(gas_df)
+    predictByClassfiers(gas_df)
+
+    # T-test
+    tTest(df)
     
 
     
